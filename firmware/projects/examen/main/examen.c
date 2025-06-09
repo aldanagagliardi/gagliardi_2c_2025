1537b8cbb70460a871199df47d376466a883e41e
Aldana Gagliardi <aldanagagliardi1997@gmail.com>
2025-06-09 15:44:50 -0300

    Generacion del proyecto y se sube el driver

---
+++ firmware/projects/examen/main/examen.c
@@ -0,0 +1,37 @@
+/*! @mainpage Examen 9-06-2025
+ *
+ * @section genDesc General Description
+ *
+ * Consigna de Aplicación: estación de monitoreo
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 09-06-2025 | Creacion del documento                         |
+ *
+ * @author Aldana Gagliardi aldanagagliardi1997@gmail.com
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	printf("Hello world!\n");
+}
+/*==================[end of file]============================================*/
